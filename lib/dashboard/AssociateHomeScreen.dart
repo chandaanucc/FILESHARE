1953dd2270e729fef133336ef0a486fcfe91e8c9
chandaanucc <chandaanucc@gmail.com>
2024-06-27 15:36:14 +0530

    url change

--- lib/dashboard/AssociateHomeScreen.dart
+++ lib/dashboard/AssociateHomeScreen.dart
@@ -34,7 +34,8 @@ class _HomeScreen1State extends State<AssociateHomeScreen> {
   // }
 
   Future<void> logout(BuildContext context) async {
-    final url = Uri.parse('http://10.0.2.2:5036/api/Login/logout'); // Use the correct logout API endpoint
+    final url = Uri.parse(
+        'http://10.0.2.2:5036/api/Login/logout'); // Use the correct logout API endpoint
     // final url = Uri.parse('http://127.0.0.1:5036/api/Login/logout');
     // final url = Uri.parse('http://localhost:5036/api/Login/logout');
 
@@ -60,14 +61,10 @@ class _HomeScreen1State extends State<AssociateHomeScreen> {
 
   final List<String> images = [
     "assets/flags/plus.png",
-  
   ];
 
   final List<String> imageData = [
-   
     "View Pdf",
-
-   
   ];
 
   @override
@@ -85,164 +82,165 @@ class _HomeScreen1State extends State<AssociateHomeScreen> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: Column(
-                children: [
-                  Container(
-                    height: 400,
-                    width: width,
-                    child: Padding(
-                      padding: const EdgeInsets.only(
-                        top: 100, // Adjusted top padding for "ADMIN" text
-                        left: 30,
-                        bottom: 50,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          const Row(
+        child: Stack(children: [
+          Positioned.fill(
+            child: Column(
+              children: [
+                Container(
+                  height: 400,
+                  width: width,
+                  child: Padding(
+                    padding: const EdgeInsets.only(
+                      top: 100, // Adjusted top padding for "ADMIN" text
+                      left: 30,
+                      bottom: 50,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'ASSOCIATE',
+                              style: TextStyle(
+                                color: Color(0xff66fcf1),
+                                fontSize: 45,
+                                letterSpacing: 2.0,
+                                fontWeight: FontWeight.w900,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Spacer(),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            left: 0,
+                            bottom: 15, // Slightly reduced bottom padding
+                          ),
+                          child: Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Text(
-                                'ASSOCIATE',
-                                style: TextStyle(
-                                  color: Color(0xff66fcf1),
-                                  fontSize: 45,
-                                  letterSpacing: 2.0,
-                                  fontWeight: FontWeight.w900,
-                                ),
+                              Image.asset(
+                                "assets/flags/homelogo.png",
+                                color: const Color(0xFF66FCF1),
+                                width: 140,
+                                height: 140,
                               ),
                             ],
                           ),
-                          const Spacer(),
-                          Padding(
-                            padding: const EdgeInsets.only(
-                              left: 0,
-                              bottom: 15, // Slightly reduced bottom padding
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: height * 0.7,
+                    decoration: const BoxDecoration(
+                      color: Color(0xffC5C6C7),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(30),
+                        topRight: Radius.circular(30),
+                      ),
+                    ),
+                    child: GridView.builder(
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1.0,
+                        mainAxisSpacing: 15,
+                        crossAxisSpacing: 30,
+                      ),
+                      padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
+                      itemCount: imageData.length,
+                      itemBuilder: (context, index) {
+                        return InkWell(
+                          onTap: () {
+                            if (index == 0) {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const ViewScreen()),
+                              );
+                              //   } else if (index == 1) {
+                              //     Navigator.push(
+                              //       context,
+                              //       MaterialPageRoute(builder: (context) => UploadScreen()),
+                              //     );
+                              //   } else if (index == 2) {
+                              //     Navigator.push(
+                              //       context,
+                              //       MaterialPageRoute(builder: (context) => AssoHomeScreen()),
+                              //     );
+                              //   }
+                              // },
+                            }
+                          },
+                          child: Container(
+                            height: 250,
+                            width: 10,
+                            margin: const EdgeInsets.symmetric(
+                                vertical: 8, horizontal: 4),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: const Color.fromARGB(255, 8, 4, 23),
+                              boxShadow: [
+                                BoxShadow(
+                                  spreadRadius: 1,
+                                  blurRadius: 6,
+                                  color: Colors.black.withOpacity(0.1),
+                                ),
+                              ],
                             ),
-                            child: Row(
+                            child: Column(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
                                 Image.asset(
-                                  "assets/flags/homelogo.png",
-                                  color: const Color(0xFF66FCF1),
-                                  width: 140,
-                                  height: 140,
+                                  images[index],
+                                  width: 50,
+                                  height: 50,
+                                  color: const Color(0xff45A29E),
+                                  fit: BoxFit.contain,
+                                ),
+                                const SizedBox(height: 10),
+                                Text(
+                                  imageData[index],
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                    color: Color(0xffC5C6C7),
+                                    fontWeight: FontWeight.bold,
+                                  ),
                                 ),
                               ],
                             ),
                           ),
-                        ],
-                      ),
+                        );
+                      },
                     ),
                   ),
-                  Expanded(
-                    child: Container(
-                      height: height * 0.7,
-                      decoration: const BoxDecoration(
-                        color: Color(0xffC5C6C7),
-                        borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(30),
-                          topRight: Radius.circular(30),
-                        ),
-                      ),
-                      child: GridView.builder(
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          childAspectRatio: 1.0,
-                          mainAxisSpacing: 15,
-                          crossAxisSpacing: 30,
-                        ),
-                        padding: const EdgeInsets.fromLTRB(20, 60, 20, 20),
-                        itemCount: imageData.length,
-                        itemBuilder: (context, index) {
-                          return InkWell(
-                            onTap: () {
-                              if (index == 0) {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(builder: (context) => const ViewScreen()),
-                                );
-                            //   } else if (index == 1) {
-                            //     Navigator.push(
-                            //       context,
-                            //       MaterialPageRoute(builder: (context) => UploadScreen()),
-                            //     );
-                            //   } else if (index == 2) {
-                            //     Navigator.push(
-                            //       context,
-                            //       MaterialPageRoute(builder: (context) => AssoHomeScreen()),
-                            //     );
-                            //   }
-                            // },
-                              }
-                            },
-                            child: Container(
-                              height: 250,
-                              width: 10,
-                              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 4),
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: const Color.fromARGB(255, 8, 4, 23),
-                                boxShadow: [
-                                  BoxShadow(
-                                    spreadRadius: 1,
-                                    blurRadius: 6,
-                                    color: Colors.black.withOpacity(0.1),
-                                  ),
-                                ],
-                              ),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Image.asset(
-                                    images[index],
-                                    width: 50,
-                                    height: 50,
-                                    color: const Color(0xff45A29E),
-                                    fit: BoxFit.contain,
-                                  ),
-                                  const SizedBox(height: 10),
-                                  Text(
-                                    imageData[index],
-                                    style: const TextStyle(
-                                      fontSize: 16,
-                                      color: Color(0xffC5C6C7),
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: 25,
-              right: 20,
-              child: Column(children:[IconButton(
-                icon: const Icon(
-                  Icons.logout,
-                  color: Color(0xFF66FCF1),
                 ),
-                onPressed: () => logout(context),
-              ),
-              const Text('Logout', style:TextStyle(fontSize: 10,
-               color:Color(0xFF66FCF1))
-              ),
-              
-      ],
-      ),
-            
-      )
-    ]),
+              ],
+            ),
+          ),
+          Positioned(
+            top: 25,
+            right: 20,
+            child: Column(
+              children: [
+                IconButton(
+                  icon: const Icon(
+                    Icons.logout,
+                    color: Color(0xFF66FCF1),
+                  ),
+                  onPressed: () => logout(context),
+                ),
+                const Text('Logout',
+                    style: TextStyle(fontSize: 10, color: Color(0xFF66FCF1))),
+              ],
+            ),
+          )
+        ]),
       ),
     );
   }
